5a7ad72f397c065d6ba1cf463fc09cc5e4257be4
Ritesh Kumar <kushwaharitesh123456@gmail.com>
2025-06-27 21:58:45 +0530

    Resolved merge conflict

--- popup.js
+++ popup.js
@@ -5,3 +5,11 @@ function showMessage() {
 function hideMessage() {
     document.getElementById("popup").style.display = "none";
 }
+
+function showPhoto() {
+    document.getElementById("xyz").style.display = "flex";
+}
+
+function hidePhoto() {
+    document.getElementById("xyz").style.display = "none";
+}
